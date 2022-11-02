7a2c55474b500a568281b6c04d2a917f40fc6ef2
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2022-11-02 22:53:13 +0100

    update

--- folder1/Tinatable.js
+++ folder1/Tinatable.js
@@ -11,6 +11,7 @@ Tinatable.update = (config, navstate) =>
 
 	if (config.html.table)
 	{
+		console.assert(config.html.table instanceof HTMLTableElement);
 		// Removes the element from the DOM.
 		config.html.table.remove();
 	}
@@ -20,20 +21,14 @@ Tinatable.update = (config, navstate) =>
 		return;
 	}
 
-
 	config.html.table = document.createElement("table");
-	let components = navstate.o; // Get sequence of components
-	config.html.thead = thead_fill(config.scope, components, Nav.cb_search, Global.navstate);
-	config.html.table.appendChild(config.html.thead);
-
-
-	
-
 	config.prom = config.backend.fetcher_get(navstate).then((r) =>
 	{
-		//console.log("Fetched: ", r);
 		navstate.p = r.cursor;
+		let components = navstate.o; // Get sequence of components
+		config.html.thead = thead_fill(config.scope, components, Nav.cb_search, Global.navstate);
 		config.html.tbody = tbody_fill(config.scope, r.rows, components);
+		config.html.table.appendChild(config.html.thead);
 		config.html.table.appendChild(config.html.tbody);
 	});
 
--- folder1/index.js
+++ folder1/index.js
@@ -10,6 +10,8 @@ Global.navstate["t2"] =
 };
 
 
+history.replaceState(null, null, document.location.pathname + Nav.href(Global.navstate, null));
+
 
 Global.t1 = 
 {
@@ -53,15 +55,7 @@ Promise.all([Backend.fetch_meta]).then((x) =>
 	// Value type information is used to represent data in correct graphics:
 	Global.t1.meta = x[0];
 	Global.t2.meta = x[0];
-	let h = Nav.href(Global.navstate, null);
-	if (h != window.location.hash)
-	{
-		window.location.hash = h;
-	}
-	else
-	{
-		Global.hashchange();
-	}
+	Global.hashchange();
 });
 
 
@@ -69,26 +63,19 @@ Promise.all([Backend.fetch_meta]).then((x) =>
 
 // The navstate is stored in the URL-hash.
 // Update the GUI accordingly when URL-hash has been changed:
+
 Global.hashchange = () =>
 {
 	// Everytime the URL-hash has changed we need to parse the URL-hash and update the Global navstate:
 	let h = window.location.hash.substring(1);
 	Global.navstate = JSURL.parse(h);
 
-	console.log("Global.hashchange", Global.hashchange_ignore);
-	if(Global.hashchange_ignore)
-	{
-		Global.hashchange_ignore = false;
-		return;
-	}
-
 	// Whenever navstate has changed we need to update all other href:
 	Tinatable.update(Global.t1, Global.navstate["t1"]);
 	Tinatable.update(Global.t2, Global.navstate["t2"]);
 	Promise.all([Global.t1.prom, Global.t2.prom]).then(x => {
 		Nav.update(Global.navstate);
-		Global.hashchange_ignore = true;
-		window.location.hash = Nav.href(Global.navstate, null);
+		history.replaceState(null, null, document.location.pathname + Nav.href(Global.navstate, null));
 	});
 }
 
